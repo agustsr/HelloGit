545b596167f194e92f7973fe7c6174b4490ffde1
Agustina SR <sulis.rinna@gmail.com>
2018-01-07 13:55:37 +0700

    first commit

---
+++ app/src/main/java/com/asr5xir2/hellogit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.asr5xir2.hellogit
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
